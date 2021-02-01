c314b6fc14b37b3592e90af02f36797476cbba78
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2021-02-01 16:32:00 +0100

    Create base login form. Handle form submit with inputs data

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import { route } from './router';
 
-route('/', 'home', function() {
-  this.where = 'here';
+route('/', 'home', function () {
+  this.username = '';
+  this.password = '';
+
+  this.$on('.username', 'change', (e) => {
+    this.username = e.target.value;
+  });
+
+  this.$on('.password', 'change', (e) => {
+    this.password = e.target.value;
+  });
+
+  this.$on('.loginForm', 'submit', (e) => {
+    console.log(`Form submitted with
+    username: ${this.username}
+    password: ${this.password}`);
+  });
 });
 
-route('/ex1', 'example1', function() {
-  this.title = 'Example 1';
+route('/success', 'success', function () {
+  this.title = 'Success Page';
 });
 
-route('/ex2', 'example2', function() {
+route('/ex2', 'example2', function () {
   this.title = 'Example 2';
   this.counter = 0;
   this.$on('.my-button', 'click', () => {
